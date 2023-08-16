2bf531c0b908c025bb90300f05c9ce732ec8f79f
Rodrigo Martinez <ropecos94@gmail.com>
2023-08-16 14:13:17 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
-    //variables del valor de los campos del form Login.
     const botonIngresar = document.getElementById('regBtn');
+    const loggeado = localStorage.getItem('loggedIn');
+    botonIngresar.disabled = true;
+
+    if (loggeado) {
+        window.location.href = 'index.html';
+    } else {
+        botonIngresar.addEventListener('click', function(evento){
+            if (verificarCampos()) {
+                localStorage.setItem('loggedIn', 'true');
+            } else {
+                evento.preventDefault();
+            }
+        });
+    }
     const emailInput = document.querySelector('.login input[type="email"]');
     const passwordInput = document.querySelector('.login input[type="password"]');
-    const loggeado = localStorage.getItem('loggedIn');
 
     function verificarCampos(){
         //funcion para ver si los campos se completan correctamente, devuelve true or false
@@ -32,20 +44,28 @@ document.addEventListener('DOMContentLoaded', function(){
     emailInput.addEventListener('input', actualizarBotonIngreso);
     passwordInput.addEventListener('input', actualizarBotonIngreso);
 
-    //Boton Ingresar por defecto al cargar la pagina.
-    botonIngresar.disabled = true;
 
-    if (loggeado) {
-        window.location.href = 'index.html';
-    } else {
-        botonIngresar.addEventListener('click', function(evento){
-            if (verificarCampos()) {
-                localStorage.setItem('loggedIn', 'true');
-            } else {
-                evento.preventDefault();
-            }
-        });
-    }
+
+
 
 
 });
+
+  // Debajo tenémos el MOSTRAR Y OCULTAR contraseña:
+
+  const passInput = document.getElementById("password");
+  const passButton = document.getElementById("buttonPassword");
+  const hidePass = document.getElementById("hidePassword");
+  const showPass = document.getElementById("showPassword");
+  
+  passButton.addEventListener("click", function() {
+      if (passInput.type === "password") {
+          passInput.type = "text";
+          showPass.style.display = "inline";
+          hidePass.style.display = "none";
+      } else {
+          passInput.type = "password";
+          showPass.style.display = "none";
+          hidePass.style.display = "inline";
+      }
+  });
