71a6e3ba0ab639e59926649d6be52d0dd17c0d0b
maurosoler <soler.tech.12@gmail.com>
2023-08-26 15:36:13 -0300

    Trabajando en el Search

--- js/products.js
+++ js/products.js
@@ -1,6 +1,9 @@
 const contenedor = document.getElementById('elContenedor');
+const inputSearch = document.getElementById('inputSearch');
+const formSearch = document.getElementById('formSearch');
 
 function mostrarProducto(dataArray) {
+  contenedor.innerHTML = ''; // Limpiar el contenedor antes de mostrar los resultados
     for (const item of dataArray) {
         const divDeProducto = document.createElement('div');
         divDeProducto.classList.add('divProducto','row','list-group-item','d-flex','justify-content-between')
@@ -21,6 +24,7 @@ function mostrarProducto(dataArray) {
         contenedor.appendChild(divDeProducto);
     }
 }
+// ESTE BLOQUE DEBAJO PERMITE MOSTRAR LOS PRODUCTOS DE DIVERSAS CATEGORIAS DE LA API por su ID.
 const idCat = localStorage.catID;
 const modifiedURL = `https://japceibal.github.io/emercado-api/cats_products/${idCat}.json`;
 fetch(modifiedURL)
@@ -28,4 +32,43 @@ fetch(modifiedURL)
     .then(data => {
         mostrarProducto(data.products);
     })
-    
+  
+// DEBAJO IMPLEMENTACION DE CÓDIGO PARA EL MOTOR DE BÚSQUEDA
+////////////////////////////////////////////////////////////////
+
+function filtrarProductos(searchText, dataArray) {
+    const resultados = dataArray.filter(item => {
+    const titulo = item.name.toLowerCase();
+    const descripcion = item.description.toLowerCase();
+    return titulo.includes(searchText) || descripcion.includes(searchText);
+    });
+  mostrarProducto(resultados);
+}
+
+formSearch.addEventListener('input', function (event) {
+  const searchText = inputSearch.value.toLowerCase();
+  filtrarProductos(searchText, productos);
+  event.preventDefault(); // Evitar el envío del formulario
+});
+
+// Datos de ejemplo (reemplaza con tus datos reales)
+
+ const productos = [
+  {
+    name: "onix",
+    currency: 'USD',
+    cost: 10,
+    description: 'Descripción del producto 1',
+    soldCount: 5,
+    image: '../img/prod50921_1.jpg'
+  },
+  {
+    name: 'Producto 2',
+    currency: 'USD',
+    cost: 20,
+    description: 'Descripción del producto 2',
+    soldCount: 10,
+    image: 'imagen2.jpg'
+  },
+  // Agrega más productos aquí...
+]; 
