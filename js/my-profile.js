7a0a1d7318865aa185cfdde3e74d8ef9533b3b98
Alvaro <alvarodc99@hotmail.com>
2023-10-31 15:43:40 -0300

    desafiate entrega 7

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,11 +1,46 @@
+const imageInput = document.getElementById('imgInput');
+const displayImage = document.getElementById('displayImage');
+const deleteImage = document.getElementById('deleteImage');
+
 userEmailFunction();
 themeFunction();
 
- userEmail = localStorage.getItem("email");
+const userEmail = localStorage.getItem('email');
 
- if(!userEmail){
+if (!userEmail) {
     window.location.href = 'index.html';
- }
- const emailInput = document.getElementById("email");
+}
 
- emailInput.value = userEmail
+const emailInput = document.getElementById('email');
+emailInput.value = userEmail;
+
+//Cargar foto si existe
+if (localStorage.getItem('image')) {
+    displayImage.src = localStorage.getItem('image');
+}
+
+// Guardar foto de perfil en localStorage
+imageInput.addEventListener('change', (event) => {
+    const image = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+        localStorage.setItem('image', reader.result);
+
+        displayImage.src = reader.result;
+    });
+
+    if (image) {
+        reader.readAsDataURL(image);
+    }
+});
+
+// Borrar foto de perfil
+deleteImage.addEventListener('click', () => {
+   const confirmation = confirm('Borrar foto de perfil?');
+
+   if (confirmation) {
+       localStorage.removeItem('image');
+       window.location.reload();
+   }
+});
