6edce00beb33caaea818b7dd578b1bff26ff9ed5
[MelaniePorley] <melanie.porley@gmail.com>
2023-11-01 17:08:11 -0300

    product quantity validations fixed

--- js/cart.js
+++ js/cart.js
@@ -157,7 +157,7 @@ async function fetchDataAndShow() {
       <input class="cart-quantity" type="number" value="1" max="999" min="1" class="text-center">
     </div>
     <div class="col-6 col-sm-2 text-end text-sm-center fw-bold p-0">USD <span class="subtotal">${price.toFixed(2)}</span></div>
-    <div class="col-1 col-sm-1 p-0 text-center p-0 removeItem" title="Eliminar del carrito" data-productID="${productID}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-trash3" viewBox="0 0 22 22">
+    <div class="col-1 col-sm-1 p-0 text-center p-0 removeItem" title="Eliminar del carrito" data-productID="${productID}"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-trash3" viewBox="0 0 16 16">
     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
   </svg></div>
   `;
@@ -256,7 +256,6 @@ function removeProductFromCart(productID) {
   updateTotalSum();
 }
 
-let selectedQuantity = 1;
 const arrayCartStandar = [];
 
 /* 
@@ -389,6 +388,43 @@ function validateCardExpiration(cardExpiration){
   };
 };
 
+function checkProductQuantity(){
+  const productQuantityInputs = document.querySelectorAll('.cart-quantity');
+  console.log(productQuantityInputs);
+  let inputsChecked = [];
+  for(const productQuantityInput of productQuantityInputs) { 
+ if(ProductQuantityValidation(productQuantityInput.value)) {
+ const inputNotValidated = {
+  check: true
+ };
+inputsChecked.push(inputNotValidated);
+ } else {
+  const inputValidated = {
+    check: false
+  };
+  inputsChecked.push(inputValidated);
+ }
+  }
+  return inputsChecked;
+};
+
+function validateInputsChecked() {
+ const inputsChecked = checkProductQuantity()
+ if(inputsChecked.find(input => input.check === false)){
+  return false;
+ } else {
+  return true;
+ }
+};
+
+function ProductQuantityValidation(value){
+  if(value !== '' && value >= 1){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 //Función para realizar las validaciones previas a la compra y aplicar estilos con bootstrap 
 (()=> {
   'use strict'
@@ -501,7 +537,7 @@ if(accountNumber.value === ''){
 }
 }
 
-if(streetAddress.value !== '' && numberAddress.value !== '' && cornerAddress.value !== '' && selectedOption !== "disabled"  && selectedQuantity <= 1 && validateMethodPayment(methodPaymentSelected)) {
+if(streetAddress.value !== '' && numberAddress.value !== '' && cornerAddress.value !== '' && selectedOption !== "disabled" && validateMethodPayment(methodPaymentSelected) && validateInputsChecked()) {
   form.classList.add('was-validated');
   divAlert.innerHTML += `
   <div id="successAlert" class="alert alert-success" role="alert" style="z-index: 1;">
